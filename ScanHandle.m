809e3d0c8a71be1325623e803319d38f067992b5
Vedant Dave <83164901+VedantDave1997@users.noreply.github.com>
2021-06-17 01:00:18 +0200

    Add files via upload
    Adding Reactive Obstacle Avoidance

---
+++ ScanHandle.m
@@ -0,0 +1,27 @@
+classdef ScanHandle < handle
+   
+ %POSEHANDLE Summary of this class goes here
+    
+%   Detailed explanation goes here
+    
+ 
+   
+properties
+        
+Ranges_m=[];
+        
+Angles_rad=[];
+        
+    
+end
+    
+ 
+   
+methods
+    
+end
+  
+  
+
+end
+
