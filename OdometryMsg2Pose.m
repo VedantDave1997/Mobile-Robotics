13b5422a489246a53072d319ca3899df952172ba
vedan <vedantdave14@gmail.com>
2021-06-17 01:12:43 +0200

    Deleting Unnecessary Files

--- OdometryMsg2Pose.m
+++
@@ -1,9 +0,0 @@
-function [ x, y, theta ] = OdometryMsg2Pose( poseMsg )
-% convert odometry message to vector
-% 2007 - Frank Hoffmann
-x=poseMsg.Pose.Pose.Position.X;
-y=poseMsg.Pose.Pose.Position.Y;
-eul=quat2eul([poseMsg.Pose.Pose.Orientation.W poseMsg.Pose.Pose.Orientation.X  poseMsg.Pose.Pose.Orientation.Y poseMsg.Pose.Pose.Orientation.Z]);
-theta=eul(1);
-end
-
